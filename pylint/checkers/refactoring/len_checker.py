304297c6222cd706737c232f7e85707ffc3c95ca
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-09-05 22:11:53 +0200

    Handle range, generator list/dict/set comprehension

--- pylint/checkers/refactoring/len_checker.py
+++ pylint/checkers/refactoring/len_checker.py
@@ -80,7 +80,6 @@ class LenChecker(checkers.BaseChecker):
         # could be if, while, assert or if expression statement
         # e.g. `if len(S):`
         if _is_call_of_name(node, "len"):
-
             # the len() call could also be nested together with other
             # boolean operations, e.g. `if z or len(x):`
             parent = node.parent
@@ -89,15 +88,22 @@ class LenChecker(checkers.BaseChecker):
             # we're finally out of any nested boolean operations so check if
             # this len() call is part of a test condition
             if _is_test_condition(node, parent):
-                instance = next(node.args[0].infer())
+                try:
+                    instance = next(node.args[0].infer())
+                except astroid.InferenceError:
+                    self.add_message("len-as-condition", node=node)
+                    return
                 mother_classes = self.base_classes_of_node(instance)
                 affected_by_pep8 = any(
-                    t in mother_classes for t in ["str", "tuple", "range", "list"]
+                    t in mother_classes for t in ["str", "tuple", "list", "set"]
                 )
-                if affected_by_pep8 and not self.instance_has_bool(instance):
+                if "range" in mother_classes or (
+                    affected_by_pep8 and not self.instance_has_bool(instance)
+                ):
                     self.add_message("len-as-condition", node=node)
 
-    def instance_has_bool(self, class_def: astroid.ClassDef) -> bool:
+    @staticmethod
+    def instance_has_bool(class_def: astroid.ClassDef) -> bool:
         return any(hasattr(f, "name") and f.name == "__bool__" for f in class_def.body)
 
     @utils.check_messages("len-as-condition")
